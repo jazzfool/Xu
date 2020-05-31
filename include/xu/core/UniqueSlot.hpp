c4bba2e81b978dccacc45e8e731b2e505fa6232e
jazzfool <saveuselon@gmail.com>
2020-05-31 18:46:23 +1000

    fix UniqueSlot

--- include/xu/core/UniqueSlot.hpp
+++ include/xu/core/UniqueSlot.hpp
@@ -23,36 +23,105 @@
 #pragma once
 
 #include "nano_signal_slot.hpp"
+#include <vector>
 
 namespace xu {
+
+template <typename... Ts> class Signal;
+namespace detail {
+template <typename... Ts> struct SlotForSignal { Signal<Ts...> *signal; };
+} // namespace detail
+
+template <typename... Ts> class Signal final {
+public:
+  using SignalT = Nano::Signal<void(Ts...)>;
+
+  Signal() : nextSlotKey{0} {}
+
+  ~Signal() {
+    for (auto const &slot : uniqueSlots) {
+      if (slot)
+        slot->signal = nullptr;
+    }
+  }
+
+  SignalT *operator->() { return &sig; }
+  SignalT &operator*() { return sig; }
+  const SignalT *operator->() const { return &sig; }
+  const SignalT &operator*() const { return sig; }
+
+private:
+  template <auto X> friend class UniqueSlot;
+
+  std::uint32_t AddSlot(detail::SlotForSignal<Ts...> *slot) {
+    uniqueSlots.push_back(slot);
+    return uniqueSlots.size() - 1;
+  }
+
+  inline void SetSlot(std::uint32_t slot, detail::SlotForSignal<Ts...> *value) {
+    uniqueSlots[slot] = value;
+  }
+
+  SignalT sig;
+  std::uint32_t nextSlotKey;
+  std::vector<detail::SlotForSignal<Ts...> *> uniqueSlots;
+};
 
 // RAII-like slot object that automatically disconnects the slot from the signal
 // when destructed.
 template <auto X> class UniqueSlot final {};
 
-template <typename T, typename... Ts, void (T::*F)(Ts...)> class UniqueSlot<F> {
+template <typename T, typename... Ts, void (T::*F)(Ts...)>
+class UniqueSlot<F> : private detail::SlotForSignal<Ts...> {
 public:
-  UniqueSlot(Nano::Signal<void(Ts...)> &sig, T *obj) : signal{&sig}, obj{obj} {
+  UniqueSlot(Signal<Ts...> &sig, T *obj)
+      : signal{&sig}, obj{obj}, slotKey{sig.AddSlot(this)} {
     signal->template connect<F>(obj);
   }
 
-  ~UniqueSlot() { signal->template disconnect<F>(obj); }
+  UniqueSlot(UniqueSlot &&other)
+      : signal{other.signal}, obj{other.obj}, slotKey{other.slotKey} {
+    other.signal = nullptr;
+    other.obj = nullptr;
+    signal->SetSlot(slotKey, this);
+  }
 
-  Nano::Signal<void(Ts...)> *signal;
+  UniqueSlot(const UniqueSlot &) = delete;
+
+  ~UniqueSlot() {
+    if (signal)
+      signal->template disconnect<F>(obj);
+  }
+
+  UniqueSlot &operator=(const UniqueSlot &) = delete;
 
 private:
   T *obj;
+  std::uint32_t slotKey;
 };
 
-template <typename... Ts, void (*F)(Ts...)> class UniqueSlot<F> {
+template <typename... Ts, void (*F)(Ts...)>
+class UniqueSlot<F> : private detail::SlotForSignal<Ts...> {
 public:
   UniqueSlot(Nano::Signal<void(Ts...)> &sig) : signal{&sig} {
     signal->template connect<F>();
   }
 
-  ~UniqueSlot() { signal->template disconnect<F>(); }
+  UniqueSlot(UniqueSlot &&other) : signal{other.signal} {
+    other.signal = nullptr;
+  }
 
-  Nano::Signal<void(Ts...)> *signal;
+  UniqueSlot(const UniqueSlot &) = delete;
+
+  ~UniqueSlot() {
+    if (signal)
+      signal->template disconnect<F>();
+  }
+
+  UniqueSlot &operator=(const UniqueSlot &) = delete;
+
+private:
+  std::uint32_t slotKey;
 };
 
 } // namespace xu
