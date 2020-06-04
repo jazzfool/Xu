2bdcba3c7d2177a1b325d9eb52abeab282fc9b56
jazzfool <saveuselon@gmail.com>
2020-06-04 20:48:01 +1000

    documentation (fixes #4)

--- include/xu/core/Layout.hpp
+++ include/xu/core/Layout.hpp
@@ -36,6 +36,11 @@ namespace xu {
 class Widget;
 class Layout;
 
+/*!
+ * \brief Mediating class which acts as a proxy for anything that can be placed
+ * in a layout (namely other layouts and widgets).
+ * \sa Layout
+ */
 class XU_API LayoutItem final {
 public:
     LayoutItem() = default;
@@ -44,10 +49,28 @@ public:
     LayoutItem& operator=(LayoutItem const&) = delete;
     LayoutItem& operator=(LayoutItem&&) = default;
 
+    /*!
+     * \brief Changes the geometry rectangle of the underlying object.
+     * \sa LayoutItem::Rect
+     */
     void SetRect(FRect2 const& rect);
+    /*!
+     * \brief Returns the geometry rectangle of the underlying object.
+     * \sa LayoutItem::SetRect
+     */
     FRect2 Rect() const;
+    /*!
+     * \brief Returns the preferred size of the underlying object. For widgets
+     * this is Widget::SizeHint, and for layouts this is Layout::MinSize.
+     * \sa Widget::SizeHint, Layout::MinSize
+     */
     FSize2 PreferredSize() const;
 
+    /*!
+     * \brief Returns the hidden state of this object. That is to say; should
+     * its rectangle be considered in the layout.
+     * \sa Widget::hidden
+     */
     bool Hidden() const;
 
 private:
@@ -61,30 +84,92 @@ private:
     std::variant<Widget*, std::unique_ptr<Layout>> item;
 };
 
+/*!
+ * \brief Minimal layout interface which is capable of selecting a minimum
+ * fitting size and changing the geometry of its items when invalidated. The
+ * implementor of this interface is responsible for storing children
+ * via Layout::InsertItem and Layout::NumItems.
+ * \sa LayoutItem, Widget
+ */
 class XU_API Layout {
 public:
     virtual ~Layout();
 
+    /*!
+     * \brief Computes the minimum size required by this layout to fit all its
+     * item. Normally this is a rectangular convex hull.
+     * \sa Widget::SizeHint
+     */
     virtual FSize2 MinSize() const = 0;
+    /*!
+     * \brief Number of items present in this layout.
+     */
     virtual std::size_t NumItems() const = 0;
 
+    /*!
+     * \brief Updates the geometry for each item in whatever manner deemed
+     * suitable by the implementation. The underlying implementation method is
+     * only called if the layout has been invalidated (the invalidation process
+     * is handled automatically).
+     * \sa LayoutItem::SetRect
+     */
     virtual void Update(FRect2 const& rect) final;
+    /*!
+     * \brief Forcefully invalidates this layout. Note: This does not call
+     * Layout::Update.
+     */
     virtual void Invalidate() final;
 
+    /*!
+     * \brief Insert a widget item at a certain index.
+     * \sa AddWidget, InsertLayout
+     */
     virtual void InsertWidget(std::size_t where, Widget* widget) final;
+    /*!
+     * \brief Adds a widget item onto the end.
+     * \sa InsertWidget, AddLayout
+     */
     virtual void AddWidget(Widget* widget) final;
 
+    /*!
+     * \brief Insert a layout item at a certain index.
+     * \sa AddLayout, InsertWidget
+     */
     virtual void InsertLayout(
         std::size_t where, std::unique_ptr<Layout> layout) final;
+    /*!
+     * \brief Adds a layout item onto the end.
+     * \sa InsertLayout, AddWidget
+     */
     virtual void AddLayout(std::unique_ptr<Layout> layout) final;
 
+    /*!
+     * \brief Changes the layout geometry. This is the same geometry that is
+     * typically used by the implementor to infer the size available for item
+     * geometry layout.
+     * \sa Geometry
+     */
     virtual void SetGeometry(FRect2 const& geometry) final;
 
+    /*!
+     * \brief Returns the layout geometry.
+     * \sa SetGeometry
+     */
     virtual FRect2 Geometry() const final;
 
 protected:
+    /*!
+     * \brief Request for an item insertion at a certain index.
+     */
     virtual void InsertItem(std::size_t where, LayoutItem item) = 0;
+    /*!
+     * \brief Updates all the stored LayoutItem geometry.
+     * \sa LayoutItem::SetRect
+     */
     virtual void UpdateItems() = 0;
+    /*!
+     * \brief Invoked whenever SetGeometry is invoked.
+     */
     virtual void OnGeometryChanged() {}
 
 private:
--- include/xu/core/Signal.hpp
+++ include/xu/core/Signal.hpp
@@ -27,6 +27,12 @@
 
 namespace xu {
 
+/*!
+ * \brief Signal/slot type which emits events with variadic template arguments
+ * (hence the variadic templates). Slots can be connected with member function
+ * pointers or regular function pointers.
+ * \sa UniqueSlot
+ */
 template<typename... Ts>
 class Signal final {
 public:
@@ -36,37 +42,71 @@ public:
         }
     }
 
+    /*!
+     * \brief Connects a member function pointer callback (the first template
+     * parameter) with a pointer to an instance of the class (the first and only
+     * parameter).
+     */
     template<auto MemFnPtr, typename T>
     void Connect(T* instance) {
         sig.template connect<MemFnPtr, T>(instance);
     }
 
+    /*!
+     * \brief Connects a member function pointer callback (the first template
+     * parameter) with a reference to an instance of the class (the first and
+     * only parameter).
+     */
     template<auto MemFnPtr, typename T>
     void Connect(T& instance) {
         sig.template connect<MemFnPtr, T>(instance);
     }
 
+    /*!
+     * \brief Connects a function pointer callback (the first template
+     * parameter).
+     */
     template<auto FnPtr>
     void Connect() {
         sig.template connect<FnPtr>();
     }
 
+    /*!
+     * \brief Disconnects a previously connected callback. This is an exact
+     * reverse operation of Connect and thus has the same parameters.
+     */
     template<auto MemFnPtr, typename T>
     void Disconnect(T* instance) {
         sig.template disconnect<MemFnPtr, T>(instance);
     }
 
+    /*!
+     * \brief Disconnects a previously connected callback. This is an exact
+     * reverse operation of Connect and thus has the same parameters.
+     */
     template<auto MemFnPtr, typename T>
     void Disconnect(T& instance) {
         sig.template disconnect<MemFnPtr, T>(instance);
     }
 
+    /*!
+     * \brief Disconnects a previously connected callback. This is an exact
+     * reverse operation of Connect and thus has the same parameters.
+     */
     template<auto FnPtr>
     void Disconnect() {
         sig.template disconnect<FnPtr>();
     }
 
+    /*!
+     * \brief Emits an event with specified arguments, essentially invoking all
+     * the connected callbacks.
+     */
     inline void Emit(Ts&&... arg) { sig.fire(std::forward<Ts>(arg)...); }
+    /*!
+     * \brief Same function as Emit.
+     * \sa Emit
+     */
     inline void operator()(Ts&&... arg) { sig.fire(std::forward<Ts>(arg)...); }
 
 private:
